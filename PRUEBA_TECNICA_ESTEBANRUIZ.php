591645b42dc00ed3b2a4eb9661956dd89a4a89b6
estebanruiz1296 <91762347+estebanruiz1296@users.noreply.github.com>
2025-01-23 14:06:56 -0500

    funcion array_map
    Se crean 3 funciones con el fin de obtener los valores pares de un array, los valores duplicados siempre y cuando sean pares y la suma total de los valores pares.

--- PRUEBA_TECNICA_ESTEBANRUIZ.php
+++ PRUEBA_TECNICA_ESTEBANRUIZ.php
@@ -14,4 +14,44 @@
         }
         return "La tipo " . $tipo . " no coincide con ninguno de los alojamientos";
     }
+
+
+
+    //MANEJO DE ARRAYS
+    $arrayNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+    //array_map 
+    echo "<br/> array_map pares";
+    function arrayMapPares ($value) {
+        if($value % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+    print_r(array_map("arrayMapPares", $arrayNumbers));
+
+
+
+    echo "<br/> array_map pares duplicados";
+    function arrayMapParesDuplicados ($value) {
+        if($value % 2 == 0){
+            return $value * 2;
+        }
+        return $value;
+    }
+    print_r(array_map("arrayMapParesDuplicados", $arrayNumbers));
+
+
+    echo "<br/> array_map pares suma";
+    function arrayMapParesSuma ($value) {
+        static $sumaPares = 0;
+        if($value % 2 == 0){
+            $sumaPares += $value;
+            return $sumaPares;
+        }
+        // return $value;
+    }
+    print_r(array_map("arrayMapParesSuma", $arrayNumbers));
+
+    
 ?>
